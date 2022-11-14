7281f92663d932becca3f3409ff3d9a240de7787
sasarkarimi <sahar.karimi90@outlook.com>
2022-11-14 23:17:24 +0000

    Update exercises folder

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,33 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+
+let [itemName,quantity,unitPrice]=order;
+
+let calcTotalOrderPrice = function (order) {
+  return order.reduce((sum, item) => {
+    return sum + item.unitPrice * item.quantity;
+    
+  }, 0);
+};
+
+
+const totalPrice=calcTotalOrderPrice(order);
+
+function createOrderRecipt(order){
+  let title = `QTY     ITEM                TOTAL`;
+  console.log(title);
+
+  for(let item of order){
+    console.log(
+      `${item.quantity}       ${item.itemName}           ${(
+        (item.unitPrice * item.quantity * 100) /
+        100
+      ).toFixed(2)}`
+    );
+  }
+  console.log(`Total : ${totalPrice}`);
+}
+
+createOrderRecipt(order);
